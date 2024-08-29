081ca0fe02d1cbaf31bd5fe973bc0f688428ad93
hyperspace42 <kivs00@list.ru>
2024-08-29 10:44:27 +0300

    initial commit

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import '../app.css';
+</script>
+
+<h1 class="text-indigo-600 text-2xl">Welcome to SvelteKit</h1>
+<p class="text-zinc-200">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-2xl text-orange-600">Привет мир</p>
